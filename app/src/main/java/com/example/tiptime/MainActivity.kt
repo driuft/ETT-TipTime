0fd67df4284cf9273b33fbe436b273019c440a02
Aldo Socarras <air.socarras@gmail.com>
2021-10-03 00:28:32 -0400

    Fixed formatting and applied best practices

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -8,7 +8,7 @@ import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,21 +19,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
         if (cost == null) {
             binding.tipResult.text = ""
             return
         }
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
         var tip = cost * tipPercentage
-        val roundUp = binding.roundUpSwitch.isSelected
-        if (roundUp) {
+        if (binding.roundUpSwitch.isSelected) {
             tip = ceil(tip)
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
